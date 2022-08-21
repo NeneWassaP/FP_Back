d0c584c8464651bb354467a0a4dc839904ee409f
NeneWassaP <nene.nesaki@gmail.com>
2022-08-22 00:48:20 +0700

    mediapipe

--- mediapipe/predict.js
+++ mediapipe/predict.js
@@ -5,7 +5,7 @@ const multer  = require('multer');
 const upload = multer({ dest: './collect' });
 const fs = require('fs');
 const path = require('path');
-const { user,collect } = require('./../models');
+const { user, word , track ,collect } = require('./../models');
 const { tokenVerificationMiddleware } = require('./../middleware');
 const { Op } = require('sequelize');
 const axios = require('axios');
@@ -48,9 +48,44 @@ collect.destroy({
 
 //console.log(holistic.Solution())
 
-
-router.post('/predict',upload.single('image'),async (req, res) => {
+router.post("/question", tokenVerificationMiddleware, async (req,res) => {
+    const { unit } = req.body;
+    const check = await track.findOne({
+        attributes: ["unit_id"],
+        where: {
+            user_id: req.user.id,
+        },
+    });
     
+    if(unit>check.unit_id){
+        return res.status(400).json({ message: "This unit isn't unlocked!!"});
+    }
+
+    const Qnum = [];
+    let num;
+    for(let i=0; i<3 ; i++){
+        do{
+            num = Math.floor(Math.random()*6)+(6*(unit-1))+1;
+        }while(Qnum.includes(num))
+        Qnum.push(num);       
+    }
+    
+    let result = await word.findAll({
+        attributes: ['id','word'],
+        where: {
+            id: {
+                [Op.in]: Qnum
+            }
+        }
+    });
+    
+    res.json({question: result})
+});
+
+router.post('/predict',[tokenVerificationMiddleware,upload.single('image')],async (req, res) => {
+    
+    const { word_id } = req.body;
+
     await collect.create({
         user_id: 1,
         file_name: req.file.filename
@@ -59,7 +94,7 @@ router.post('/predict',upload.single('image'),async (req, res) => {
     const count = (await collect.findAll({
         attributes: ["file_name"],
         where:{
-            user_id: 1
+            user_id: req.user.id,
         },
         order: [
             ["id","desc"]
@@ -71,11 +106,21 @@ router.post('/predict',upload.single('image'),async (req, res) => {
 
     const response = await axios.post("http://localhost:3001/python", { file_name: req.file.filename, count });
     
-    return res.json({ message : response.data });
+    //return res.json({ message : response.data });
+
+    const check = await word.findOne({
+        attributes: ['word'],
+        where:{
+            id: word_id
+        },
+    });
+
+    is_correct = check.word == response;
+    
+    res.json({ is_correct })
+
 })
  
-
-
 
 module.exports = router;
 
